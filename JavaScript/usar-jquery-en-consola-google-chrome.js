3742a87c63539c344e17cf8e8a009dfe735051f0
Oscar Abad <62606443+Oscar-Abad-Folgueira@users.noreply.github.com>
2023-10-03 20:14:27 +0200

    Update usar-jquery-en-consola-google-chrome.js

--- JavaScript/usar-jquery-en-consola-google-chrome.js
+++ JavaScript/usar-jquery-en-consola-google-chrome.js
@@ -1,3 +1,10 @@
+/**
+ * @snippet       JavaScript: Utilizar jQuery en la consola de Google Chrome
+ * @author        Oscar Abad Folgueira
+ * @author_url    https://www.oscarabadfolgueira.com
+ * @snippet_url   https://www.aprendeinformaticaconmigo.com/desarrollo/javascript/utilizar-jquery-en-la-consola-de-google-chrome/
+ */
+
 var jq = document.createElement('script');
 jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js";
 document.getElementsByTagName('head')[0].appendChild(jq);
