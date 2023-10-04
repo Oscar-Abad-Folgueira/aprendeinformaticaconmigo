df44a28e5d17ebe9768a2b6f13d4f2bb7e488153
Oscar Abad <62606443+Oscar-Abad-Folgueira@users.noreply.github.com>
2023-10-04 09:56:49 +0200

    Create obtener-la-url-anterior.js

---
+++ JavaScript/obtener-la-url-anterior.js
@@ -0,0 +1,13 @@
+/**
+ * @snippet       JavaScript: Obtener la url de la página anterior
+ * @author        Oscar Abad Folgueira
+ * @author_url    https://www.oscarabadfolgueira.com
+ * @snippet_url   https://www.aprendeinformaticaconmigo.com/desarrollo/javascript/javascript-obtener-la-url-de-la-pagina-visitada-anteriormente
+ */
+
+// Guardar la url de la página anteior en una variable.
+// Usamos document.referrer
+let urlAnteior = document.referrer;
+
+// Mostrar por consola el contenido de la variable urlAnterior.
+console.log(urlAnterior);
